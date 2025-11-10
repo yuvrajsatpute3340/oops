43c2f16396058d810120ba1665f31817fc6a32e3
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2025-11-10 14:45:42 +0530

    java

---
+++ Array/Demo1.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Demo1 {
+	public static void main(String[]args)
+	{
+		int[]arr=new int [5];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;
+		//arr[5]=60;
+		System.out.println(arr.length);
+		
+		
+		}
+
+}
---
+++ Array/Demo2.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int []nums=new int [6];
+		nums[0]=10;
+		nums[4]=60;
+		nums[3]=50;
+		nums[2]=40;
+		nums[1]=20;
+		nums[5]=30;
+		char []chars =new char [100];
+		System.out.println(nums [2]*300);
+	}
+
+}
