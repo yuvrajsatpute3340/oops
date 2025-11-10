1b7c20d4092d1ea19f9cae3d5ed56cdf1d3f9fbb
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2025-11-10 14:58:20 +0530

    Java basic

---
+++ first/Demo.java
@@ -0,0 +1,14 @@
+package first;
+
+public class Demo {
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World");
+		
+		System.out.println(10+12);
+		
+		System.out.println(22);
+		
+	}
+
+}
