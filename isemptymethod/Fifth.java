a2bcb1706e59aff3a39d51c61f12a3940ce184dd
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2025-12-03 19:06:20 +0530

    Advance java

---
+++ isemptymethod/Fifth.java
@@ -0,0 +1,16 @@
+package isemptymethod;
+
+import java.util.ArrayList;
+
+public class Fifth {
+
+	public static void main(String[] args) {
+		
+		ArrayList arr=new ArrayList<>(4);
+		
+		System.out.println(arr.isEmpty());
+		
+		System.out.println(arr);
+	}
+
+}
