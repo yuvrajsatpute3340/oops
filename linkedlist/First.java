cb423b9aaf49e1e05ae86eadb889cdd542f9dd73
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2025-12-04 22:03:16 +0530

    Adavnvce java

---
+++ linkedlist/First.java
@@ -0,0 +1,20 @@
+package linkedlist;
+
+import java.util.LinkedList;
+
+public class First {
+	public static void main(String []args)
+	{
+		LinkedList<Integer> list=new LinkedList<Integer>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		
+		System.out.println(list.get(3));
+		
+	}
+
+}
---
+++ linkedlist/Second.java
@@ -0,0 +1,20 @@
+package linkedlist;
+
+import java.util.LinkedList;
+
+public class Second {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> list=new LinkedList<String>();
+		
+		list.add("Unique");
+		list.add("System");
+		list.add("Pvt ltd");
+		list.add("India");
+		
+		System.out.println(list.get(2));
+
+	}
+
+}
