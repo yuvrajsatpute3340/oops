7ef81ed3d61c7d7671fb315d5beb84e2a8a41d66
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2025-12-19 19:35:39 +0530

    Avance java

---
+++ setif/Treeset_2.java
@@ -0,0 +1,19 @@
+package setif;
+
+import java.util.TreeSet;
+
+public class Treeset_2 {
+
+	public static void main(String[] args) {
+		
+		TreeSet<String> set=new TreeSet<>();
+		
+		set.add("C");
+		set.add("A");
+		set.add("B");
+		
+		System.out.println(set);
+		
+	}
+
+}
