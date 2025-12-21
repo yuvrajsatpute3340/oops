b148d65038b6933fd08d0f930ee0a786604e9da5
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2025-12-21 11:52:33 +0530

    Advance java

---
+++ setif/Third.java
@@ -0,0 +1,25 @@
+package setif;
+
+import java.util.HashSet;
+
+public class Third {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> set=new HashSet<>();
+		
+		String str="abc";
+		String str2=new String("abc");
+		
+		System.out.println(str.hashCode());
+		System.out.println(str2.hashCode());
+		
+		System.out.println(str.equals(str2));
+		
+		set.add(str2);
+		set.add(str);
+		
+		System.out.println(set);
+	}
+
+}
