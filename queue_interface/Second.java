e7a1bb80518de4095235259df0077947707293fd
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2025-12-21 13:17:42 +0530

    Advance java

---
+++ queue_interface/Second.java
@@ -0,0 +1,19 @@
+package queue_interface;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Example1 {
+    public static void main(String[] args) {
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(10);
+        q.add(20);
+        q.add(30);
+
+        System.out.println(q);      // [10, 20, 30]
+        System.out.println(q.poll()); // 10
+        System.out.println(q.peek()); // 20
+    }
+}
