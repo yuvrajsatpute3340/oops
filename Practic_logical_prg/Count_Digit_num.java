036b49faf37da4f4529afd282ef2a05d39274664
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2025-12-27 19:27:34 +0530

    Count_num

---
+++ Practic_logical_prg/Count_Digit_num.java
@@ -0,0 +1,17 @@
+package Practic_logical_prg;
+
+public class Count_Digit_num {
+
+	public static void main(String[] args) {
+		
+		int num = 1234567891, count = 0;
+		while (num != 0) {
+		    count++;
+		    num /= 10;
+		}
+
+		System.out.println(count);
+
+	}
+
+}
