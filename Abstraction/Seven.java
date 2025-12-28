9319e4e494ba6bc69540d219750e2680d9b43c97
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2025-12-28 14:25:51 +0530

    Encapsulation

--- Abstraction/Seven.java
+++ Abstraction/Seven.java
@@ -4,7 +4,6 @@ public class Seven {
 
 	public static void main(String[] args) {
 		
-		System.out.println("xgc");
 		
 	}
 }
---
+++ Encapsulation/Third.java
@@ -0,0 +1,41 @@
+package Encapsulation;
+
+public class Third {
+
+	public static void main(String[] args) {
+		
+		Demo1 demo=new Demo1();
+		
+		demo.Applayname("ABCD");
+		
+		System.out.println(demo.getPrivate());
+	}
+}
+class Demo1
+{
+	private int i;
+	private String name;
+	
+	public void display()
+	{
+		
+	}
+	public String getPrivate() {
+		return null;
+	}
+	public void Applayname(String name)
+	{
+		if (name.length()>=4)
+		{
+			this.name=name;
+		}
+		else 
+		{
+			this.name=null;
+		}
+	}
+	public String getPrivate1()
+	{
+		return this.name;
+	}
+}
