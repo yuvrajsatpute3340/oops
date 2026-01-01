e91f55e91576ec23a15c7188dd4362784232e00d
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-01-01 11:52:21 +0530

    Polymorphism

---
+++ Polymorphism/Poly.java
@@ -0,0 +1,35 @@
+package Polymorphism;
+
+public class Poly {
+
+	public static void main(String[] args) {
+		
+		Calculator1 cals=new Calculator1();
+		cals.add(12,33,56);
+		
+		System.out.println();
+	}
+}
+class Calculator1
+{
+	public Calculator1()
+	{
+		
+	}
+	public Calculator1(int i)
+	{
+		
+	}
+	public void add(int i,int j)
+	{
+		System.out.println(i+j);
+	}
+	public int add(int i, int j,int k)
+	{
+		return i+j+k;
+	}
+	public void add(double d,double d2)
+	{
+		System.out.println(d+d2);
+	}
+}
