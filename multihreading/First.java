afad37ac918d138af2f86aef1dd62582165e5bff
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-02-02 13:54:17 +0530

    Multithreading_Demo

---
+++ multihreading/First.java
@@ -0,0 +1,27 @@
+package multihreading;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		int count =1;
+		while(count<100) {
+			System.out.println("Hello");
+		}
+		NumberPrinter number=new NumberPrinter();
+		number.run();
+	}
+
+}
+class NumberPrinter implements Runnable
+{
+
+	@Override
+	public void run() {
+		System.out.println("Hello runnable...");
+	}
+	
+}
+
+	
+
