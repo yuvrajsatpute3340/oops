3c05466e9533bf2c3e60b4e85f0e4f4502264cbd
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-02-09 11:06:26 +0530

    ListIf

---
+++ listif/Demo.java
@@ -0,0 +1,22 @@
+package listif;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		List<Integer>list=new ArrayList<Integer>();
+		
+		list.add(10);
+		list.add(20);
+		list.add(10);
+		list.add(20);
+		
+		System.out.println(list);
+		
+
+	}
+
+}
