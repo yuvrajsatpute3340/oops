7f269bec3ce879751544db0823809c89c9616884
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-02-10 16:24:58 +0530

    Linked_List

---
+++ linkedlist/Eight.java
@@ -0,0 +1,22 @@
+package linkedlist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Eight {
+
+	public static void main(String[] args) {
+		LinkedList<Integer>list=new LinkedList<Integer>();
+		
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		list.add(50);
+		
+		System.out.println(list.get(1));
+		
+
+	}
+
+}
