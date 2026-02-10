7407d45fff4d5e823ed320b0101d8341f35140d8
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-02-10 21:24:15 +0530

    Misiing_num

---
+++ Practic_logical_prg/MissingNum_ArrayList.java
@@ -0,0 +1,34 @@
+package Practic_logical_prg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissingNum_ArrayList {
+
+	public static void main(String[] args) {
+		
+//		List<Integer>list =new ArrayList<Integer>();
+//	       list.add(10);
+//	       list.add(12);
+//	       list.add(13);
+//	       list.add(15);
+//	       list.add(20);
+//	       
+//	       int num=list.getFirst();
+//	       for (int i =list.getFirst();i<=list.getLast();i++)
+//	       {
+//	           if (! list.contains(num))
+//	           {
+//	               System.out.println(num);
+//	           }
+//	           num++;
+	//       }
+	       
+	    
+	       
+	       
+	       
+	       
+	       
+	}
+}
