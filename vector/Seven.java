56a6126935d92ae83f26ae88efdf05f90954a5d6
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-04-07 23:08:21 +0530

    Vector

--- vector/Seven.java
+++ vector/Seven.java
@@ -1,5 +1,27 @@
 package vector;
+import java.util.Vector;
 
 public class Seven {
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<>();
 
+        v.add(10);
+        v.add(20);
+        v.add(10);
+        v.add(30);
+        v.add(20);
+        v.add(40);
+        v.add(50);
+
+        System.out.println("Original Vector: " + v);
+        Vector<Integer> unique = new Vector<>();
+
+        for(int i = 0; i < v.size(); i++) {
+            if(!unique.contains(v.get(i))) {
+                unique.add(v.get(i));
+            }
+        }
+
+        System.out.println("Unique Elements: " + unique);
+    }
 }
--- vector/Six.java
+++ vector/Six.java
@@ -1,5 +1,33 @@
 package vector;
 
-public class Six {
+import java.util.Vector;
 
+public class Six {
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<>();
+
+        v.add(11);
+        v.add(20);
+        v.add(33);
+        v.add(40);
+        v.add(55);
+
+        System.out.println("Vector elements: " + v);
+        int even = 0, odd = 0;
+
+        for(int i = 0; i < v.size(); i++) {
+            if(v.get(i) % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Even count: " + even);
+        System.out.println("Odd count: " + odd);
+        System.out.print("Reverse Vector: ");
+        for(int i = v.size() - 1; i >= 0; i--) {
+            System.out.print(v.get(i) + " ");
+        }
+    }
 }
