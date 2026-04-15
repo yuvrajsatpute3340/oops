2885c1ded0c95bbc18e517744a170b2adbb3967e
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-04-15 20:41:17 +0530

    Contain_method

---
+++ containsmethod/Seven.java
@@ -0,0 +1,26 @@
+package containsmethod;
+
+import java.util.*;
+
+public class Seven {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(2);
+        list.add(7);
+        list.add(4);
+
+        Collections.sort(list); // 🔥 important
+
+        int start = list.get(0);
+        int end = list.get(list.size() - 1);
+
+        for (int i = start; i <= end; i++) {
+            if (!list.contains(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+}
