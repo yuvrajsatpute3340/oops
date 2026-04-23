06a9745591e4a39be666bc6103409f5c782677a9
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-04-23 21:55:48 +0530

    Syn_example

---
+++ Synchronized/Eight.java
@@ -0,0 +1,5 @@
+package Synchronized;
+
+public class Eight {
+
+}
--- predicate_fun/Eight.java
+++ predicate_fun/Eight.java
@@ -1,10 +1,16 @@
 package predicate_fun;
 
+import java.util.function.Predicate;
+
 public class Eight {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Predicate<String> p1 = s -> s.length() > 5;
+		Predicate<String> p2 = s -> s.startsWith("A");
 
+		System.out.println(p1.or(p2).test("Apple"));  // true (starts with A)
+		System.out.println(p1.or(p2).test("Banana")); // true (length > 5)
+		System.out.println(p1.or(p2).test("Cat"));    // false (none match)
 	}
 
 }
