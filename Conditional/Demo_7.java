5c062847425f53911a8a6d66e7f966f5ce76aeba
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-05-19 20:29:03 +0530

    CoNdItIoNaL

--- Conditional/Demo_7.java
+++ Conditional/Demo_7.java
@@ -2,4 +2,18 @@ package Conditional;
 
 public class Demo_7 {
 
+    public static void main(String[] args) {
+
+        String course = "Java";
+        double feesPaid = 5000;
+
+        if (course.equals("Java") && feesPaid >= 5000)
+        {
+            System.out.println("Admission Confirmed");
+        }
+        else
+        {
+            System.out.println("Admission Pending");
+        }
+    }
 }
--- Conditional/Demo_8.java
+++ Conditional/Demo_8.java
@@ -1,5 +1,17 @@
 package Conditional;
-
 public class Demo_8 {
 
+    public static void main(String[] args) {
+        int batteryPercentage = 80;
+        boolean internetConnected = true;
+
+        if (batteryPercentage > 20 && internetConnected)
+        {
+            System.out.println("Video Call Started");
+        }
+        else
+        {
+            System.out.println("Unable to Start Video Call");
+        }
+    }
 }
