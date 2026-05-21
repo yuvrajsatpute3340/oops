0928b97c5ae931d37f71a5a3209acdad50fd29cf
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-05-21 20:14:45 +0530

    Condi

--- Conditional/Demo_10.java
+++ Conditional/Demo_10.java
@@ -2,4 +2,18 @@ package Conditional;
 
 public class Demo_10 {
 
+    public static void main(String[] args) {
+
+        int age = 22;
+        boolean hasLicense = true;
+
+        if (age >= 18 && hasLicense)
+        {
+            System.out.println("You Can Drive a Car");
+        }
+        else
+        {
+            System.out.println("You Cannot Drive a Car");
+        }
+    }
 }
--- Conditional/Demo_9.java
+++ Conditional/Demo_9.java
@@ -2,4 +2,18 @@ package Conditional;
 
 public class Demo_9 {
 
+    public static void main(String[] args) {
+
+        int attendance = 85;
+        boolean feesPaid = true;
+
+        if (attendance >= 75 && feesPaid)
+        {
+            System.out.println("Eligible for Exam");
+        }
+        else
+        {
+            System.out.println("Not Eligible for Exam");
+        }
+    }
 }
