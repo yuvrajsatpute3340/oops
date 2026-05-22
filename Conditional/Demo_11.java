403d6c1ee2cbe04e571274b4b817eee9c9dfea50
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-05-22 18:45:32 +0530

    Lambdas

--- Conditional/Demo_11.java
+++ Conditional/Demo_11.java
@@ -2,4 +2,18 @@ package Conditional;
 
 public class Demo_11 {
 
+    public static void main(String[] args) {
+
+        int marks = 78;
+        boolean attendanceGood = true;
+
+        if (marks >= 40 && attendanceGood)
+        {
+            System.out.println("You Passed the Exam");
+        }
+        else
+        {
+            System.out.println("You Failed the Exam");
+        }
+    }
 }
