13007ab7a0ebd203990b3936dea719ce636d40d7
yuvrajsatpute3340 <yuvrajsatpute3340@gmail.com>
2026-05-23 17:51:21 +0530

    LAMBDAS

--- lambdas/Seven.java
+++ lambdas/Seven.java
@@ -2,4 +2,24 @@ package lambdas;
 
 public class Seven {
 
+    public static void main(String[] args) {
+
+        // Lambda Expression
+        I_Bill<Integer, String> bill = (item1, item2, item3) -> {
+
+            int total = item1 + item2 + item3;
+
+            return "Total Shopping Bill = " + total;
+        };
+
+        String result = bill.generateBill(250, 400, 150);
+
+        System.out.println(result);
+    }
+}
+
+// Functional Interface
+interface I_Bill<T, R> {
+
+    R generateBill(T t1, T t2, T t3);
 }
--- lambdas/Six.java
+++ lambdas/Six.java
@@ -2,4 +2,26 @@ package lambdas;
 
 public class Six {
 
+    public static void main(String[] args) {
+
+        // Lambda expression
+        I_Calculate<Integer, String> bonusCalc = (salary, bonus, extraAllowance) -> {
+
+            int totalIncome = salary + bonus + extraAllowance;
+
+            return new StringBuffer("Total Employee Income: ")
+                    .append(totalIncome)
+                    .toString();
+        };
+
+        String result = bonusCalc.calculate(50000, 10000, 5000);
+
+        System.out.println(result);
+    }
+}
+
+// Functional Interface
+interface I_Calculate<T, R> {
+
+    R calculate(T t1, T t2, T t3);
 }
